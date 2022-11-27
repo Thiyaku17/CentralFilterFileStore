e561e2aa12f53e3e32e23427b64d2e1263b3df82
TGLoverV2 <116097665+TGLoverV2@users.noreply.github.com>
2022-11-27 18:35:23 +0530

    Ithelam Too Much uh!

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID', '1305232')
-API_HASH = environ('API_HASH', '71b917e302a36826c54988990dc7a789')
-BOT_TOKEN = environ('BOT_TOKEN', "2107710484:AAHFQgzcOlFWovDZ3gu5fO0zCNqhbyLxBcU")
+API_ID = int(environ.get('API_ID', '1305232')
+API_HASH = environ.get('API_HASH', '71b917e302a36826c54988990dc7a789')
+BOT_TOKEN = environ.get('BOT_TOKEN', "2107710484:AAHFQgzcOlFWovDZ3gu5fO0zCNqhbyLxBcU")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
