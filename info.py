404435d2df32d4bd920415957dac6b9d3e91e055
TGLoverV2 <116097665+TGLoverV2@users.noreply.github.com>
2022-11-27 18:41:01 +0530

    Missed!

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '1305232')
+API_ID = int(environ.get('API_ID', '1305232'))
 API_HASH = environ.get('API_HASH', '71b917e302a36826c54988990dc7a789')
 BOT_TOKEN = environ.get('BOT_TOKEN', "2107710484:AAHFQgzcOlFWovDZ3gu5fO0zCNqhbyLxBcU")
 
