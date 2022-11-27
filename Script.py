96398d3367567c784c2550c76f97d0dda6cdf3fd
Thiyaku17 <65347563+Thiyaku17@users.noreply.github.com>
2022-11-27 20:04:02 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,15 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
 class script(object):
     START_TXT = """<b>Hello ... {} 👋
 ✯ My Name is <a href=https://t.me/{}>{}</a>
